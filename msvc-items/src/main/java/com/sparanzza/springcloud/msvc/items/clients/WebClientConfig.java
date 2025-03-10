a47d4ef74a342b9c0290599de141ae5fb6fa30e3
Aurelio Munoz Hijano <aurelio@sparanzza.com>
2025-03-10 07:38:12 +0100

    Config BaseUrl end point

--- msvc-items/src/main/java/com/sparanzza/springcloud/msvc/items/clients/WebClientConfig.java
+++ msvc-items/src/main/java/com/sparanzza/springcloud/msvc/items/clients/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.sparanzza.springcloud.msvc.items.clients;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +9,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${config.baseurl.endpoint.msvc-products}")
+    private String url;
+
     @Bean
     @LoadBalanced
     WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+        return WebClient.builder().baseUrl(url);
     }
 }
