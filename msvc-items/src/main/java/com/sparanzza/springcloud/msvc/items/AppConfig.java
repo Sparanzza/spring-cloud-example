eb79d6e088e3777eb5b9cdea1a7bc70b17137bb4
Aurelio Munoz Hijano <aurelio@sparanzza.com>
2025-03-29 22:10:58 +0100

    Configuration Circuit Breaker

---
+++ msvc-items/src/main/java/com/sparanzza/springcloud/msvc/items/AppConfig.java
@@ -0,0 +1,27 @@
+package com.sparanzza.springcloud.msvc.items;
+
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                        .slidingWindowSize(10)
+                        .failureRateThreshold(50)
+                        .waitDurationInOpenState(Duration.ofSeconds(10))
+                        .build())
+                .build());
+    }
+
+}
