f89cf9936c45dbf14b03b5ad7b5f0664aedb8a0a
Aurelio Munoz Hijano <aurelio@sparanzza.com>
2025-04-08 06:51:35 +0200

    Added Actuactor and RefreshScop ItemController

--- msvc-items/src/main/java/com/sparanzza/springcloud/msvc/items/controllers/ItemController.java
+++ msvc-items/src/main/java/com/sparanzza/springcloud/msvc/items/controllers/ItemController.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@RefreshScope
 @RestController
 public class ItemController {
 
