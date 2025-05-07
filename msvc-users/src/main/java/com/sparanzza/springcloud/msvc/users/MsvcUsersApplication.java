659b7b41a4722ef9383e7ae8a42c8807daa802ad
Aurelio Munoz Hijano <aurelio@sparanzza.com>
2025-05-08 00:31:10 +0200

    Micro service users

--- msvc-users/src/main/java/com/sparanzza/springcloud/msvc/users/MsvcUsersApplication.java
+++ msvc-users/src/main/java/com/sparanzza/springcloud/msvc/users/MsvcUsersApplication.java
@@ -2,12 +2,20 @@ package com.sparanzza.springcloud.msvc.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsvcUsersApplication {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsvcUsersApplication.class, args);
 	}
-
 }
---
+++ msvc-users/src/main/java/com/sparanzza/springcloud/msvc/users/services/IUserService.java
@@ -0,0 +1,20 @@
+package com.sparanzza.springcloud.msvc.users.services;
+
+import java.util.Optional;
+
+import com.sparanzza.springcloud.msvc.users.entities.User;
+
+public interface IUserService {
+
+    Optional<User> findById(Long id);
+
+    Optional<User> findByUsername(String username);
+
+    Iterable<User> findAll();
+
+    User save(User user);
+
+    Optional<User> update(User user, Long id);
+
+    void delete(Long id);
+}
